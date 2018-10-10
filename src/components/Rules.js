0e93b0e4f20563069859f0df5fe53af86b14aab2
YongJo <eai.dalman@gmail.com>
2018-10-10 14:07:46 +0800

    Nothing

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-import { TabContent, TabPane, Nav, NavItem, NavLink, Row, Col } from 'reactstrap';
-import * as permissions from '../permissions/permissions'
-import Banner from './Banner';
-
-class Rules extends Component {
-    render() {
-        return (
-            <div>
-                <Banner 
-                header="Rules"
-                contents="List of rules that has been set." />
-                <div className='all_padding'>
-                
-            </div>
-        </div>
-        );
-    }
-}
-
-export default Rules;
-=======
 import React, { Component } from 'react'
 import { Container, Button } from 'reactstrap';
 import { Route, Switch } from 'react-router-dom'
@@ -64,4 +41,3 @@ class Rules extends Component {
 }
 
 export default Rules
->>>>>>> master
