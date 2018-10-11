6cf0fc530eae8728ae94e6b5c8a66a3c0c874777
tanjarce <tanjarce@yahoo.com.ph>
2018-10-12 02:54:18 +0800

    nothing

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,7 +7,7 @@ import * as API from '../services/API';
 import * as Session from '../services/session';
 import * as permissions from '../permissions/permissions';
 
-import SideBar from './Sidebar';
+import SideBar from './SideBar';
 import Accounts from './Accounts'
 //PAGES
 import Dashboard from './Dashboard'
