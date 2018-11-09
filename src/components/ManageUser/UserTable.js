47f598540405138d65d5460cc7b5572df73ff919
YongJo <eai.dalman@gmail.com>
2018-11-09 15:25:58 +0800

    Transfered Table of ManagerUser from to other Component

--- src/components/ManageUser/UserTable.js
+++ src/components/ManageUser/UserTable.js
@@ -6,8 +6,6 @@ import { UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from
 import SearchAndCount from '../SearchAndCount'
 import Tables from '../Tables'
 import '../../styles/manage.css'
-import * as API from '../../services/API'
-import * as Help from '../../toastify/helpers'
 
 import dots from '../../images/show_more.svg'
 import SuspensionModal from '../../modals/SuspensionModal'
@@ -15,130 +13,8 @@ import UserDeleteModal from '../../modals/UserDeleteModal'
 import userDefafult from '../../assets/userDefault.svg'
 
 class TableUser extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            modalType: 'delete',
-            isOpen: false,
-            dataUsers : [],
-            count : '',
-            userInfo : [],
-            idSuspend : '',
-            roleStat : '',
-            loading : true,
-            order : 'username',
-            sort : 'ASC',
-            pagination: {
-                offset: 0,
-                limit: 10
-            },
-            searchQ: ''
-        }
-        this.orderSort = this.orderSort.bind(this)
-        this.loading = this.loading.bind(this)
-        this.suspendUser = this.suspendUser.bind(this)
-        this.updateTable = this.updateTable.bind(this)
-        this.toggleModal = this.toggleModal.bind(this)
-        this.setModal = this.setModal.bind(this)
-        this.viewUser = this.viewUser.bind(this)
-    }
-    componentDidMount(){
-        this.updateTable();
-    }
-    loading(){
-        this.setState({
-            loading : true
-        })
-    }
-    updateTable(paginationData, searchQData){
-        this.setState((prevState)=>({
-            loading: true,
-            searchQ: (typeof searchQData !== 'undefined') ? searchQData.trim() : prevState.searchQ,
-            pagination: paginationData ? {...paginationData} : prevState.pagination
-        }), ()=>{
-            const { pagination, searchQ, order, sort } = this.state 
-            const data = (typeof searchQData === 'undefined')
-            ? {
-                order : order,
-                sort : sort,
-                searchQ : searchQ,
-                ...pagination
-            }
-            : {
-                order : order,
-                sort : sort,
-                searchQ: searchQ,
-                ...pagination,
-                offset: 0
-            }
-            API.getAllUser(data)
-            .then((response) => {
-                response.success === true ?
-                    
-                this.setState({
-                    dataUsers : response.users.rows,
-                    count : response.users.count,
-                    loading : false
-                })
-                :
-                console.log(response.error.message)
-            })
-        })
-
-    }
-
-    suspendUser(){        
-        const { roleStat } = this.state
-        if((roleStat.role !== 'admin' && roleStat.status !== 'inactive')){
-        const { idSuspend } = this.state
-        API.suspendUser(idSuspend)
-        .then((response) => {
-            response.success ? 
-            (
-            this.updateTable(),
-            Help.toastPop({message: `Suspended successfully`, type: 'success'})
-            )
-            :
-            Help.toastPop({message: response.error.message, type: 'error'})
-        })
-        }
-        else{
-            Help.toastPop({message: `You can't suspend this type.`, type: 'error'})
-        }
-    }
-
-    toggleModal () {
-        this.setState({
-            isOpen: !this.state.isOpen
-        })
-    }
-    viewUser (rowInfo){
-        this.props.history.push(`/mnguser/${rowInfo.id}`)
-    }
-    orderSort(order){
-        const { sort } = this.state
-            this.setState({
-                order : order,
-                sort : sort === 'ASC' ? 'DESC' : 'ASC'
-            }, this.updateTable())
-    }
-    setModal (data, type) {
-        console.log(data)
-        this.setState({
-            modalType: type,
-            userData: {...data},
-            idSuspend : data.id,
-            roleStat : {
-                role : data.type,
-                status : data.status
-            }
-        }, () => {
-            this.toggleModal()
-        }
-    )
-    }
-    render() {
-        const { dataUsers, count, pagination } = this.state;
+   render() {
+        const { dataUsers, count, pagination, isOpen, userData, modalType, loading, updateTable, orderSort, viewUser, setModal, toggleModal, suspendUser } = this.props
         
         const Users = dataUsers.map((user)=>{
             return ({
@@ -154,7 +30,7 @@ class TableUser extends Component {
         })
         const columnsRules = [
             {
-                Header: <div onClick={()=> {this.orderSort('username')}}>Account User</div>,
+                Header: <div onClick={()=> {orderSort('username')}}>Account User</div>,
                 accessor: 'username',
                 width: 200,
                 sortable : false,
@@ -173,30 +49,30 @@ class TableUser extends Component {
                 }
             },
             {
-                Header: <div onClick={()=> {this.orderSort('full_name')}}>Full Name</div>,
+                Header: <div onClick={()=> {orderSort('full_name')}}>Full Name</div>,
                 accessor: 'name',
                 sortable : false,
                 width: 200
             },
             {
-                Header: <div onClick={()=> {this.orderSort('address')}}>Address</div>,
+                Header: <div onClick={()=> {orderSort('address')}}>Address</div>,
                 accessor: 'address',
                 sortable : false,
             },
             {
-                Header: <div onClick={()=> {this.orderSort('rating')}}>Ratings</div>,
+                Header: <div onClick={()=> {orderSort('rating')}}>Ratings</div>,
                 accessor: 'ratings',
                 sortable : false,
                 width: 120
             },
             {
-                Header: <div onClick={()=> {this.orderSort('type')}}>Role</div>,
+                Header: <div onClick={()=> {orderSort('type')}}>Role</div>,
                 accessor: 'role',
                 width: 150,
                 sortable : false
             },
             {
-                Header: <div onClick={()=> {this.orderSort('status')}}>Account Status</div>,
+                Header: <div onClick={()=> {orderSort('status')}}>Account Status</div>,
                 accessor: 'account_status',
                 sortable : false,
                 width: 150
@@ -214,29 +90,28 @@ class TableUser extends Component {
                             <img with="15px" height="15px" src={dots} alt="show_more" className="m-auto"/>
                         </DropdownToggle>
                         <DropdownMenu>
-                            <DropdownItem onClick={() => {this.viewUser(rowInfo.value)}}>View</DropdownItem>
+                            <DropdownItem onClick={() => {viewUser(rowInfo.value)}}>View</DropdownItem>
                             {/* <DropdownItem onClick={() => {this.setModal(rowInfo.value, 'suspend') }}>Suspend</DropdownItem> */}
-                            <DropdownItem onClick={()=>{this.setModal(rowInfo.value, 'delete')}}>Suspend</DropdownItem>
+                            <DropdownItem onClick={()=>{setModal(rowInfo.value, 'delete')}}>Suspend</DropdownItem>
                         </DropdownMenu>
                     </UncontrolledDropdown>
                 )
             }]
         
-        const { isOpen, userData, modalType, loading } = this.state
         const modal = (modalType === 'suspend') 
-            ? (<SuspensionModal isOpen={isOpen} toggle={this.toggleModal} userData={userData} />)
-            : (<UserDeleteModal suspendUser={this.suspendUser} isOpen={isOpen} toggle={this.toggleModal} userData={userData} />)
+            ? (<SuspensionModal isOpen={isOpen} toggle={toggleModal} userData={userData} />)
+            : (<UserDeleteModal suspendUser={suspendUser} isOpen={isOpen} toggle={toggleModal} userData={userData} />)
         
         return (
             <React.Fragment>
             { modal }
-                <SearchAndCount updateTable={this.updateTable} text="Users" count={count}/>
+                <SearchAndCount updateTable={updateTable} text="Users" count={count}/>
                 <Tables
                     loading={loading}
                     columns={columnsRules} 
                     dataCount={count}
                     paginationData={pagination}
-                    updateTable={this.updateTable} 
+                    updateTable={updateTable} 
                     data={Users} />
             </React.Fragment>
         );
