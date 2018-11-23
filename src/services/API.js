96a633a0394addd062d35845bdf309e870494525
tanjarce <tanjarce@yahoo.com.ph>
2018-11-23 09:50:00 +0800

    merging to master

--- src/services/API.js
+++ src/services/API.js
@@ -1,8 +1,8 @@
 import * as Session from './session';
 // const api = 'http://192.168.0.92:3000/api/v1';
-const api = 'http://192.168.0.125:3000/api/v1';
+// const api = 'http://192.168.0.125:3000/api/v1';
 // const api = 'https://bingwit-backend.herokuapp.com/api/v1';
-// const api = 'http://18.224.2.191/api/v1';
+const api = 'http://18.224.2.191/api/v1';
 
 const headers = () => {
   const token = Session.getToken()
